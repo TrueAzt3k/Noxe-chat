7e828ef3a8006b8a845d8c3cdec9a314e4993633
TrueAzt3k <trueazt3k@gmail.com>
2019-08-18 14:22:50 -0500

    estructura

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <title>Blog personal</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="cabecera">
+            <p class="tema">Mi blog personal</p>
+            <div class="box">
+                <input type="submit" value="Inicio">
+                <input type="submit" value="Información">
+                <input type="submit" value="Fotos">
+                <input type="submit" value="Redes">
+                <input type="submit" value="Apoyo">
+            </div>
+        </div>
+        <div class="log" method="post">
+            <p>Loggeate</p>
+            <div class="datos">
+                <input type="text" placeholder="Usuario">
+                <p><input type="password" placeholder="Contraseña"></p>
+                <p><input type="submit" value="Loggear"></p>
+            </div>
+        </div>
+    </body>
+</html>
